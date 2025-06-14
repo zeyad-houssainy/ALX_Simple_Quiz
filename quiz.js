a03a75519f64709c97c6f0bbb36f08c7631e7a12
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-15 02:56:47 +0300

    1. Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+    let correctAnswer  = 4
+    // console.log("let's starts")
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked')
+    let userAnswer = selectedAnswer.value
+    // console.log(selectedAnswer)
+
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done." 
+    } else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!" 
+    }
+}
+
+const submitButton = document.getElementById("submit-answer")
+
+if (submitButton) {
+    submitButton.addEventListener("click", checkAnswer)
+}
