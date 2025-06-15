a55ee80f41bf74ad86c61abb4310acf18eb41f94
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-15 03:08:33 +0300

    2. Build a Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,31 @@
+const { parseSync } = require("@babel/core");
+
+// Implement Arithmetic Functions
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+// Attach Event Listeners.
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById("subtract").addEventListener("click", () => {
+    const number1 = parseFloat(document.getElementbyId("number1").value) || 0;
+    const number2 = parseFloat(document.getElementbyId("number2").value) || 0;
+})
